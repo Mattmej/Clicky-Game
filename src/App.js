fafef1bdfd0f73318ac960f68050fd23b442f690
unknown <mattcmej@gmail.com>
2018-07-16 04:03:08 -0700

    Create ClickCard component and cardArrays js file. Set up framework in App.js. Install bootstrap and reactstrap.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,53 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+import cardImages from "./cardArrays";
+import cardNames from "./cardArrays";
+
+// class App extends Component {
+//   render() {
+//     return (
+//       <div className="App">
+//         <header className="App-header">
+//           <img src={logo} className="App-logo" alt="logo" />
+//           <h1 className="App-title">Welcome to React</h1>
+//         </header>
+//         <p className="App-intro">
+//           To get started, edit <code>src/App.js</code> and save to reload.
+//         </p>
+//       </div>
+//     );
+//   }
+// }
+
+
+const App = () => (
+  <Wrapper>
+    <Title>Clicky Game!</Title>
+
+    <ClickCard />
+    <ClickCard />
+    <ClickCard />
+    <ClickCard />
+
+    <ClickCard />
+    <ClickCard />
+    <ClickCard />
+    <ClickCard />
+
+    <ClickCard />
+    <ClickCard />
+    <ClickCard />
+    <ClickCard />
+
+  </Wrapper>
+
+
+
+
+
+
+)
 
 export default App;
---
+++ src/cardArrays.js
@@ -0,0 +1,30 @@
+const cardImages = {
+    Charmander:"https://cdn.bulbagarden.net/upload/thumb/7/73/004Charmander.png/250px-004Charmander.png",
+    Squirtle:"https://cdn.bulbagarden.net/upload/thumb/3/39/007Squirtle.png/250px-007Squirtle.png",
+    Bulbasaur:"https://cdn.bulbagarden.net/upload/thumb/2/21/001Bulbasaur.png/250px-001Bulbasaur.png",
+    Cyndaquil:"https://cdn.bulbagarden.net/upload/thumb/9/9b/155Cyndaquil.png/250px-155Cyndaquil.png",
+    Totodile:"https://cdn.bulbagarden.net/upload/thumb/d/df/158Totodile.png/250px-158Totodile.png",
+    Chikorita:"https://cdn.bulbagarden.net/upload/thumb/b/bf/152Chikorita.png/250px-152Chikorita.png",
+    Torchic:"https://cdn.bulbagarden.net/upload/thumb/9/91/255Torchic.png/250px-255Torchic.png",
+    Mudkip:"https://cdn.bulbagarden.net/upload/thumb/6/60/258Mudkip.png/250px-258Mudkip.png",
+    Treecko:"https://cdn.bulbagarden.net/upload/thumb/2/2c/252Treecko.png/250px-252Treecko.png",
+    Chimchar:"https://cdn.bulbagarden.net/upload/thumb/7/76/390Chimchar.png/250px-390Chimchar.png",
+    Piplup:"https://cdn.bulbagarden.net/upload/thumb/b/b1/393Piplup.png/250px-393Piplup.png",
+    Turtwig:"https://cdn.bulbagarden.net/upload/thumb/5/5c/387Turtwig.png/250px-387Turtwig.png"
+};
+
+const cardNames = Object.keys(cardImages);
+// ["Charmander", "Squirtle", ...]
+/* 
+To select name, 
+
+cardNames[0]
+or
+cardNames[cardNames.indexOf("Charmander")];
+
+
+
+*/
+
+export default cardImages;
+export default cardNames;
