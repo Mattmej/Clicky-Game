85945eb0990692c0686e4d8a0010bbcecd558e49
unknown <mattcmej@gmail.com>
2018-07-17 04:03:58 -0700

    Create a new file called 'CardData.js' for better-formatted card data.

---
+++ src/components/CardData.js
@@ -0,0 +1,63 @@
+const CardData = [
+    {
+        name: "Charmander",
+        picture: "https://cdn.bulbagarden.net/upload/thumb/7/73/004Charmander.png/250px-004Charmander.png"
+    },
+
+    {
+        name: "Squirtle",
+        picture: "https://cdn.bulbagarden.net/upload/thumb/3/39/007Squirtle.png/250px-007Squirtle.png"
+    },
+
+    {
+        name: "Bulbasaur",
+        picture: "https://cdn.bulbagarden.net/upload/thumb/2/21/001Bulbasaur.png/250px-001Bulbasaur.png"
+    },
+
+    {
+        name: "Cyndaquil",
+        picture: "https://cdn.bulbagarden.net/upload/thumb/9/9b/155Cyndaquil.png/250px-155Cyndaquil.png"
+    },
+
+    {
+        name: "Totodile",
+        picture: "https://cdn.bulbagarden.net/upload/thumb/d/df/158Totodile.png/250px-158Totodile.png"
+    },
+
+    {
+        name: "Chikorita",
+        picture: "https://cdn.bulbagarden.net/upload/thumb/b/bf/152Chikorita.png/250px-152Chikorita.png"
+    },
+
+    {
+        name: "Torchic",
+        picture: "https://cdn.bulbagarden.net/upload/thumb/9/91/255Torchic.png/250px-255Torchic.png"
+    },
+
+    {
+        name: "Mudkip",
+        picture: "https://cdn.bulbagarden.net/upload/thumb/6/60/258Mudkip.png/250px-258Mudkip.png"
+    },
+
+    {
+        name: "Treecko",
+        picture: "https://cdn.bulbagarden.net/upload/thumb/2/2c/252Treecko.png/250px-252Treecko.png"
+    },
+
+    {
+        name: "Chimchar",
+        picture: "https://cdn.bulbagarden.net/upload/thumb/7/76/390Chimchar.png/250px-390Chimchar.png"
+    },
+
+    {
+        name: "Piplup",
+        picture: "https://cdn.bulbagarden.net/upload/thumb/b/b1/393Piplup.png/250px-393Piplup.png"
+    },
+
+    {
+        name: "Turtwig",
+        picture: "https://cdn.bulbagarden.net/upload/thumb/5/5c/387Turtwig.png/250px-387Turtwig.png"
+    },
+];
+
+export default CardData;
---
+++ src/components/CardDisplay.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import ClickCard from "./ClickCard";
+
+class CardDisplay extends Component {
+
+    // shuffle = () => {
+        
+    // }
+}
+
+export default Counter;
