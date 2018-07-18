308a22892167910a95ffce78ae81d6e3b3b4ddf1
unknown <mattcmej@gmail.com>
2018-07-18 14:49:28 -0700

    Add a Fisher-Yates shuffle function into App.js file. Will use whenever card is clicked.

--- src/App.js
+++ src/App.js
@@ -77,6 +77,27 @@ let cardData = [
 
 
 class App extends React.Component {
+
+    // Fisher-Yates shuffle
+  shuffleCards = (array) => {
+    let m = array.length, t, i;
+
+    // While there remain elements to shuffle…
+    while (m) {
+  
+      // Pick a remaining element…
+      i = Math.floor(Math.random() * m--);
+  
+      // And swap it with the current element.
+      t = array[m];
+      array[m] = array[i];
+      array[i] = t;
+    }
+  
+    return array;
+  }
+
+
   render() {
     return(
       <Container>
